1cf90b2b5c12bb6cb366e5e9e18b3c42141cae9f
Murat Tunc <murat.tunc8558@gmail.com>
2024-12-18 23:18:40 +0300

    mutu3

--- back-end/build-tools/server_droplet_setup.sh
+++ back-end/build-tools/server_droplet_setup.sh
@@ -196,7 +196,13 @@ server {
     index index.html;
 
     location / {
-        try_files $uri $uri/ =404;
+        try_files $uri /index.html;
+    }
+
+    location ~* \.(?:ico|css|js|gif|jpe?g|png|woff2?|eot|ttf|svg|mp4|webm|ogv|webp)$ {
+        expires 6M;
+        access_log off;
+        add_header Cache-Control "public";
     }
 }
 NGINX_CONF
